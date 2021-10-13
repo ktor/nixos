7c35bb9225bc11719f5ade3f71d4a56c837f356c
Pawel Kruszewski <kruszewsky@gmail.com>
2021-10-13 15:01:49 +0200

    feat: add node10 to lukreo shell

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
   environment.systemPackages = with pkgs; [
     (pkgs.buildFHSUserEnv {
       name = "lukreo";
-      targetPkgs = pkgs: (with pkgs; [ gradle adoptopenjdk-hotspot-bin-8 gnumake zlib zlib libGL gitAndTools.gitFull bash-completion bash direnv ]) ++ (with pkgs.xorg; [ libXi libXxf86vm libX11 ]);
+      targetPkgs = pkgs: (with pkgs; [ gradle adoptopenjdk-hotspot-bin-8 gnumake zlib zlib libGL gitAndTools.gitFull bash-completion bash direnv nodejs-10_x ]) ++ (with pkgs.xorg; [ libXi libXxf86vm libX11 ]);
       multiPkgs = pkgs: (with pkgs; [ ]);
       runScript = ''
         env SHELL_NAME="lukreo" bash --rcfile <(cat /home/ktor/.bashrc; echo 'source "${pkgs.gitAndTools.gitFull}/share/git/contrib/completion/git-completion.bash"; source "${pkgs.gitAndTools.gitFull}/share/git/contrib/completion/git-prompt.sh";')
